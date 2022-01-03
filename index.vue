fd65782692339d8e7ea65eee5f3b7124dbc720a2
glamourboys <glamourboys@qq.com>
2022-01-04 00:04:29 +0800

    测试提交

---
+++ index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+  export default {
+    name: "Index",
+    data() {
+      return {
+        msg: "Hello",
+      };
+    },
+  };
+</script>
+
+<style>
+</style>
